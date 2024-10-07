b53a226d7a450862a840dc14b8719ab24fdaa5d7
mrkamronbey <owenmichael0117@gmail.com>
2024-10-07 13:05:28 +0500

    change telegram account name

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -95,7 +95,7 @@ export const Page: React.FC = () => {
                 <span>Telefon orqali</span>
               </a>
               <a
-                href="https://t.me/Abdujabbor_Mukhiddinov"
+                href="https://t.me/DrAbdujabbor"
                 className={
                   "text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition flex items-center justify-center gap-2"
                 }
